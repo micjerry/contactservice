895de3a611eaf9c6289ba87f95a12c71aa14b7a9
micjerry <hansfei@126.com>
2015-11-13 10:12:05 +0800

    add total_count

--- handlers/listdevices.py
+++ handlers/listdevices.py
@@ -61,7 +61,16 @@ class ListDeviceHandler(BaseHandler):
 
                 rs_devices.append(device)
 
-            self.write({"devices": rs_devices})
+            total_count = {}
+            if rs_devices:
+                for item in rs_devices:
+                    model = item.get("model", "")
+                    if total_count.get(model, ""):
+                       total_count[model] = total_count[model] + 1
+                    else:
+                       total_count[model] = 1
+
+            self.write({"devices": rs_devices, "total_count": total_count})
 
         else:
             logging.error("user %s was not found" % userid)
